8627895de1d3964c8c32890d663ecadcf4e53e7b
musicEnfanthen <stefan.muennich@unibas.ch>
2024-05-27 15:17:25 +0200

    test(contact): fix import in test

--- src/app/views/contact/contact-imprint/contact-imprint.component.spec.ts
+++ src/app/views/contact/contact-imprint/contact-imprint.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RouterLinkStubDirective } from '@testing/router-link-stub.directive';
 

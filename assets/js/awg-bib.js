709135aec42e6f50254981e715155d84aa036e54
musicEnfanthen <stefan.muennich@unibas.ch>
2025-05-22 19:21:46 +0200

    fix(assets): improve display of pages and series in bib

--- assets/js/awg-bib.js
+++ assets/js/awg-bib.js
@@ -56,8 +56,14 @@ function processPublicationDetails(text) {
     text = text.replace(/, editor\(s\),/g, ' (Hg.),');
     // Replace pages with S.
     text = text.replace(/, pages/g, ', S.');
-    // Change order of publisher and address
-    text = text.replace(/\.\s([^,]+),\s*([^,]+),\s*(\d{4})\./, '$2: $1, $3.');
+    // Move pages to the end
+    text = text.replace(/, S\. ([^.,]+)\.\s*([^.,]+),\s*([^,]+),\s*(\d{4})\./, ', $3: $2, $4, S. $1.');
+    // Change order of publisher and address if not already handled
+    text = text.replace(/\.\s*([^.,]+),\s*([^.,]+),\s*(\d{4})\.$/, '. $2: $1, $3. ');
+    // Replace series in papers
+    text = text.replace(/, volume ([^,]+), of ([^,]+),/g, ' (= $2 $1),');
+    // Replace series in books
+    text = text.replace(/Volume ([^]+) of ([^.]+)./g, ' (= $2 $1),');
 
     return text;
 }
